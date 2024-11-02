a3530201d3c33045f9939243e749793007fe012a
mrichardsJBOIT <14255080+mrichardsJBOIT@users.noreply.github.com>
2024-11-02 23:38:20 +1100

    Remove commented-out code from create_event_command
    Cleaned up the `create_event_command.rb` by removing legacy commented-out code. This enhances code readability and maintainability by eliminating unnecessary clutter.

--- app/commands/events/create_event_command.rb
+++ app/commands/events/create_event_command.rb
@@ -12,13 +12,5 @@ class CreateEventCommand < BaseCommand
     event = Event.new(@params)
     event.organizer = @organizer
     event.save!
-      # return event
-      # @params[:ticket_types].flatten.each do |ticket_type_params|
-      #   event.ticket_types.create!(ticket_type_params)
-      # end
-      # EventCreatedJob.perform_later(event)
-    # else
-    #   errors.full_messages.each { |message| errors.add(:base, message) }
-    # end
   end
 end
